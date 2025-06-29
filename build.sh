dfeb6473797333b5e36bf643df6662642f304249
Vishalgupta7518 <vishalgupta916931@gmail.com>
2025-06-29 22:11:16 +0530

    removed unused build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install PHP dependencies
-composer install --optimize-autoloader --no-dev
-
-# Cache config
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
